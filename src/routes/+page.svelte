70f4815a762ed8f1ee9fc3d8a1cdcbd2d28c20d6
kat <katwang7749@gmail.com>
2026-08-28 17:44:21 -0400

    add most content

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,81 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+</script>
+
+<div>
+	<!-- hero -->
+	<div class="flex h-screen w-screen flex-col items-center justify-center gap-2">
+		<img
+			src="/logo-with-caption.png"
+			alt="hack club's storyboard: for teens 13-18"
+			class="m-4 w-1/3"
+		/>
+		<h1>make a visual novel, get cool prizes!</h1>
+		<div>
+			<input type="text" class="outline" />
+			<button>OK!</button>
+		</div>
+	</div>
+	<!-- how does storyboard work? -->
+	<div class="flex flex-col items-center justify-center gap-2 text-center">
+		<h1>HOW DOES STORYBOARD WORK?</h1>
+		<h2>learn how to make a visual novel:</h2>
+		<div class="grid w-2/3 grid-cols-3 grid-rows-2">
+			<div class="how-container">1</div>
+			<div class="how-container">2</div>
+			<div class="how-container">3</div>
+			<div class="how-container">4</div>
+			<div class="how-container">5</div>
+			<div class="how-container">6</div>
+		</div>
+		<h2>and we'll send you prizes!</h2>
+		<!-- scrolling prizes add them later tho -->
+		<div class="flex flex-row flex-nowrap gap-4"></div>
+		<h2>we have step-by-step guides to help:</h2>
+		<div class="grid w-5/12 grid-cols-2">
+			<div class="guide-container">
+				<p>hi</p>
+				<p>hi</p>
+			</div>
+			<div class="guide-container">2</div>
+			<div class="guide-container">3</div>
+			<div class="guide-container">4</div>
+			<div class="guide-container">5</div>
+			<div class="guide-container">6</div>
+		</div>
+		<a href="placeholder">view more →</a>
+		<h2>and a collection of <a href="placeholder">free resources</a> for you to use! :)</h2>
+	</div>
+	<!-- examples & faq -->
+	<div class="flex flex-col gap-2 items-center justify-center text-center">
+		<!-- examples -->
+		<div>
+			<h1>example projects</h1>
+			<p>Look at these cool projects other Storyboarders have made & get inspired!</p>
+			<br />Stuck on ideas? explore the <a href="placeholder">#storyboard</a> channel & feel free to
+			ask questions.
+			<!-- TV w projects - make a componentn later or sum -->
+			<div></div>
+		</div>
+		<!-- faq -->
+		<div>
+			<h1>FAQ</h1>
+			<!-- (embed w renpy game) -->
+			<h2>
+				More questions? ask in <a href="placeholder">#storyboard-help</a> or email
+				<a href="mailto:kat@hackclub.com">kat@hackclub.com</a>
+			</h2>
+		</div>
+	</div>
+	<!-- footer -->
+	<div class="flex flex-col gap-2 items-center justify-center text-center">
+		<h1>made with ♡ by teens, for teens</h1>
+		<!-- team -->
+		<div></div>
+		<p>
+			<a href="https://hackclub.com">hack club</a> ✦ <a href="https://slack.hackclub.com">slack</a>
+			✦ <a href="https://clubs.hackclub.com">clubs</a> ✦
+			<a href="https://conduct.hackclub.com">code of conduct</a>
+		</p>
+		<p>this website is <a href="https://github.com/ikealoverkat/storyboard-v3">open source!</a></p>
+	</div>
+</div>
