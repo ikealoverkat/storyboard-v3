f583756cba1b93bafa3a13086ff233d995e42921
kat <katwang7749@gmail.com>
2026-08-29 04:22:03 -0400

    adjust placeholder page for mobile

--- src/routes/placeholder/+page.svelte
+++ src/routes/placeholder/+page.svelte
@@ -8,18 +8,29 @@
 <div
 	class="placeholder flex h-screen w-screen flex-col items-center justify-center bg-[url('/bg-window.png')] bg-cover text-center"
 >
-	<div class="z-10 flex flex-col items-center justify-center gap-4">
-		<button
-			onclick={goBack}
-			class="text-glow text-xl text-teal underline hover:decoration-wavy"><i>← back</i></button
+	<div class="z-10 flex flex-col items-center justify-center gap-4 p-12">
+		<button onclick={goBack} class="text-glow text-xl text-teal underline hover:decoration-wavy"
+			><i>← back</i></button
 		>
-		<h1 class="text-glow text-4xl text-whiteish">looks like this page doesn't exist yet.</h1>
-		<p class="text-glow text-2xl text-yellow">
+		<br />
+		<h1 class="text-glow text-2xl text-pretty text-whiteish md:text-3xl lg:text-4xl">
+			looks like this page doesn't exist yet.
+		</h1>
+		<br />
+		<p class="text-glow text-lg text-pretty text-yellow md:text-xl lg:text-2xl">
 			it will soon though!! once storyboard fully launches, you shouldn't be able to find this page.
+			<br />
 			<br />if you do, PLSPLSPLS dm
-			<a href="https://hackclub.enterprise.slack.com/team/U08H34LLQQJ">@kat</a>
+			<a
+				href="https://hackclub.enterprise.slack.com/team/U08H34LLQQJ"
+				class="text-pink-purple underline hover:decoration-wavy">@kat</a
+			>
 			on slack or email
-			<a href="mailto:kat@hackclub.com">kat@hackclub.com</a> and tell her how you got here.
+			<a href="mailto:kat@hackclub.com" class="text-pink-purple underline hover:decoration-wavy"
+				>kat@hackclub.com</a
+			>
+			and tell her how you got here.
+			<br />
 			<br /> uh oh....
 		</p>
 	</div>
